454628d67bfa94329e22582597d9f85a2838a779
sundazekiks <laqkiks@gmail.com>
2026-09-25 18:21:57 -0400

    finished: the method for fetching and hydrating the dashboard page with a list of bookingsfor the user

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,7 +1,19 @@
+import Confirmation from "../models/schemas/confirmation.js"
+
 const standard_dashboard = (req, res) => {
     const user = req.session.user
-
-    res.render('user_dashboard/dashboard', { title: "Standard User Dashboard", ...user })
+    res.render('dashboard/dashboard', { title: "Standard User Dashboard", ...user })
 }
 
-export { standard_dashboard }
+const dashboard_bookings = async (req, res) => {
+    try {
+        const email = req.session.user.email;
+        const bookings = await Confirmation.find({ "passengers.email": email })
+        return res.status(200).json(bookings)
+    } catch (err) {
+        console.error(err)
+        return res.status(404).json([])
+    }
+}
+
+export { standard_dashboard, dashboard_bookings }
--- src/routes/ejs-routes.js
+++ src/routes/ejs-routes.js
@@ -1,16 +1,17 @@
+import { dashboard_bookings } from '../controllers/dashboard.js';
 import { tripDetailsPage, tripListPage } from '../controllers/trips.js';
 import { bookingPage, processBookingRequest } from './book.js';
 import confirmationPage from './confirm.js';
 import { Router } from 'express';
 
 const router = Router();
+router.get('/user-booking', dashboard_bookings)
 
 router.get('/', tripListPage);
 router.get('/booking/:scheduleId', bookingPage);
 router.post('/book', processBookingRequest);
 router.get('/confirmation/:confirmationId', confirmationPage);
 router.get('/:tripId', tripDetailsPage);
-
 
 
 export default router;
